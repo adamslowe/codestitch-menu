45dd21aa6b466619ec0a6bc0fc4cc31e7433e85c
adamslowe <lowe.adam@gmail.com>
2024-04-03 17:33:14 -0400

    Removed unnecessary arrow key handling

--- codestitch-nav.js
+++ codestitch-nav.js
@@ -76,7 +76,6 @@ class DisclosureNav {
         this.rootNode = domNode;
         this.controlledNodes = [];
         this.openIndex = null;
-        this.useArrowKeys = true;
 
         this.topLevelNodes = [...this.rootNode.querySelectorAll("button.cs-li-link[aria-expanded][aria-controls]")];
 
@@ -98,7 +97,6 @@ class DisclosureNav {
                     // attach event listeners
                     menu.addEventListener("keydown", this.onMenuKeyDown.bind(this));
                     node.addEventListener("click", this.onButtonClick.bind(this));
-                    node.addEventListener("keydown", this.onButtonKeyDown.bind(this));
                 }
             }
             // handle links
@@ -109,35 +107,6 @@ class DisclosureNav {
         });
 
         this.rootNode.addEventListener("focusout", this.onBlur.bind(this));
-    }
-
-    controlFocusByKey(keyboardEvent, nodeList, currentIndex) {
-        switch (keyboardEvent.key) {
-            case "ArrowUp":
-            case "ArrowLeft":
-                keyboardEvent.preventDefault();
-                if (currentIndex > -1) {
-                    var prevIndex = Math.max(0, currentIndex - 1);
-                    nodeList[prevIndex].focus();
-                }
-                break;
-            case "ArrowDown":
-            case "ArrowRight":
-                keyboardEvent.preventDefault();
-                if (currentIndex > -1) {
-                    var nextIndex = Math.min(nodeList.length - 1, currentIndex + 1);
-                    nodeList[nextIndex].focus();
-                }
-                break;
-            case "Home":
-                keyboardEvent.preventDefault();
-                nodeList[0].focus();
-                break;
-            case "End":
-                keyboardEvent.preventDefault();
-                nodeList[nodeList.length - 1].focus();
-                break;
-        }
     }
 
     // public function to close open menu
@@ -159,44 +128,8 @@ class DisclosureNav {
         this.toggleExpand(buttonIndex, !buttonExpanded);
     }
 
-    /*onMenuMouseEnter(event) {
-        var button = event.target;
-        var buttonIndex = this.topLevelNodes.indexOf(button);
-        var buttonExpanded = button.getAttribute("aria-expanded") === "true";
-        this.toggleExpand(buttonIndex, !buttonExpanded);
-    }*/
-
     onMenuMouseLeave(event) {
         this.toggleExpand(this.openIndex, false);
-    }
-
-    onButtonKeyDown(event) {
-        var targetButtonIndex = this.topLevelNodes.indexOf(document.activeElement);
-
-        // close on escape
-        if (event.key === "Escape") {
-            this.toggleExpand(this.openIndex, false);
-        }
-
-        // move focus into the open menu if the current menu is open
-        else if (this.useArrowKeys && this.openIndex === targetButtonIndex && event.key === "ArrowDown") {
-            event.preventDefault();
-            this.controlledNodes[this.openIndex].querySelector("a").focus();
-        }
-
-        // handle arrow key navigation between top-level buttons, if set
-        else if (this.useArrowKeys) {
-            this.controlFocusByKey(event, this.topLevelNodes, targetButtonIndex);
-        }
-    }
-
-    onLinkKeyDown(event) {
-        var targetLinkIndex = this.topLevelNodes.indexOf(document.activeElement);
-
-        // handle arrow key navigation between top-level buttons, if set
-        if (this.useArrowKeys) {
-            this.controlFocusByKey(event, this.topLevelNodes, targetLinkIndex);
-        }
     }
 
     onMenuKeyDown(event) {
@@ -211,11 +144,6 @@ class DisclosureNav {
         if (event.key === "Escape") {
             this.topLevelNodes[this.openIndex].focus();
             this.toggleExpand(this.openIndex, false);
-        }
-
-        // handle arrow key navigation within menu links, if set
-        else if (this.useArrowKeys) {
-            this.controlFocusByKey(event, menuLinks, currentIndex);
         }
     }
 
@@ -248,10 +176,6 @@ class DisclosureNav {
             }
         }
     }
-
-    updateKeyControls(useArrowKeys) {
-        this.useArrowKeys = useArrowKeys;
-    }
 }
 
 /* Initialize Disclosure Menus */
@@ -266,21 +190,10 @@ window.addEventListener(
             disclosureMenus[i] = new DisclosureNav(menus[i]);
         }
 
-        // listen to arrow key checkbox
-        var arrowKeySwitch = document.getElementById("arrow-behavior-switch");
-        if (arrowKeySwitch) {
-            arrowKeySwitch.addEventListener("change", function () {
-                var checked = arrowKeySwitch.checked;
-                for (var i = 0; i < disclosureMenus.length; i++) {
-                    disclosureMenus[i].updateKeyControls(checked);
-                }
-            });
-        }
-
         // fake link behavior
         disclosureMenus.forEach((disclosureNav, i) => {
-            var links = menus[i].querySelectorAll('[href="#mythical-page-content"]');
-            var examplePageHeading = document.getElementById("mythical-page-heading");
+            // var links = menus[i].querySelectorAll('[href="#mythical-page-content"]');
+            // var examplePageHeading = document.getElementById("mythical-page-heading");
             for (var k = 0; k < links.length; k++) {
                 // The codepen export script updates the internal link href with a full URL
                 // we're just manually fixing that behavior here
